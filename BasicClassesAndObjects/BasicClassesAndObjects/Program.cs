6e99613668f146cdcc72f6e779ab8188c05b2576
James Gilsenan PC <jamesgilsenan37@gmail.com>
2020-09-24 16:13:57 +0100

    Basic classes and objects

---
+++ BasicClassesAndObjects/BasicClassesAndObjects/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Basic_Classes_and_Objects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Forest11OverloadingConstructors f = new Forest11OverloadingConstructors("Amazon", "Tropical");
+
+            Console.WriteLine(f.Trees);
+            f.Grow();
+            Console.WriteLine(f.Trees);
+            f.Grow();
+            Console.WriteLine(f.Trees);
+            f.Burn();
+            Console.WriteLine(f.Trees);
+            Console.WriteLine(f.age);
+
+            Console.Read();
+        }
+    }
+}
