f74bccee6784733da9ba687cc544c1047e41c15e
James Gilsenan PC <jamesgilsenan37@gmail.com>
2020-09-25 20:53:55 +0100

    Demo of using classes and instanciating objects

---
+++ DatingApp/DatingApp/Profile.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace DatingProfile
+{
+    class Profile
+    {
+        private string name;
+        private int age;
+        private string city;
+        private string country;
+        private string pronouns;
+        private string[] hobbies;
+
+        public Profile(string name, int age, string city = "N/A", string country = "N/A", string pronouns = "they/them")
+        {
+            this.name = name;
+            this.age = age;
+            this.city = city;
+            this.country = country;
+            this.pronouns = pronouns;
+            this.hobbies = new string[0];
+        }
+
+        public string ViewProfile()
+        {
+            string bio = $"Name: {name}\nAge: {age}\nCity: {city}\nCountry: {country}\nPronouns: {pronouns}";
+
+            if (this.hobbies.Length > 0)
+            {
+                bio += "\n Hobbies: \n";
+                foreach (string hobby in hobbies)
+                {
+                    bio += $"- {hobby}\n";
+                }
+            }
+            return bio;
+        }
+
+        public void SetHobbies(string[] hobbies)
+        {
+            this.hobbies = hobbies;
+        }
+    }
+}
