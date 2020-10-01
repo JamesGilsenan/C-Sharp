9373338e94121f66e3b7c4ad788afb0a5a8edb03
James Gilsenan PC <jamesgilsenan37@gmail.com>
2020-10-01 16:41:55 +0100

    Preorder Traversal using recursion

---
+++ PreorderTraversal/PreorderTraversal/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PreorderTraversal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TreeNode node5 = new TreeNode(5, null, null);
+            TreeNode node4 = new TreeNode(4, null, null);
+            TreeNode node3 = new TreeNode(3, null, null);
+            TreeNode node2 = new TreeNode(2, node4, node5);
+            TreeNode node1 = new TreeNode(1, node2, node3);
+            RecursivePreorder recursive = new RecursivePreorder();
+            
+
+            IList<int> values = recursive.PreorderTraversal(node1);
+
+            foreach (int value in values)
+            {
+                Console.Write(value + ", ");
+            }
+            
+            Console.Read();
+        }
+    }
+}
---
+++ PreorderTraversal/PreorderTraversal/RecursivePreorder.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+
+namespace PreorderTraversal
+{
+    class RecursivePreorder
+    {
+        public IList<int> PreorderTraversal(TreeNode root)
+        {
+            IList<int> values = new List<int>();
+            TreeNode previous = root;
+            Helper(root, values);
+            //Helper(root, values);
+            return values;
+        }
+
+        static void Helper(TreeNode root, IList<int> values)
+        {
+            if (root != null)
+            {
+                values.Add(root.val);
+                if (root.left != null)
+                {
+                    Helper(root.left, values);
+                }
+                if (root.right != null)
+                {
+                    Helper(root.right, values);
+                }    
+            }
+        }
+    }
+}
