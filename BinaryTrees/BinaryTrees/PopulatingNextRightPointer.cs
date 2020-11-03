dc6d8d2c50cde4d5d4925919f97762b8c9c78237
James Gilsenan PC <jamesgilsenan37@gmail.com>
2020-11-03 13:31:20 +0000

    Populating Next Node

---
+++ BinaryTrees/BinaryTrees/PopulatingNextRightPointer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTrees
+{
+    class PopulatingNextRightPointer
+    {
+        //You are given a perfect binary tree where all leaves are on the same level, and every parent has two children.
+        //Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+        //Initially, all next pointers are set to NULL.
+
+        public Node Connect(Node root)
+        {
+            Node leftNode = root;
+
+            //checks there is a level below
+            while(leftNode != null && leftNode.left != null)
+            {
+                //call helper with leftNode = root to populate next pointers
+                PopulateNextHelper(leftNode);
+                //advance left node to first node in next level
+                leftNode = leftNode.left;
+            }
+
+            return root;
+        }
+
+        public void PopulateNextHelper(Node startNode)
+        {
+            Node currentNode = startNode;
+
+            //currentNode will be advanced to next node in level at end of while loop
+            while (currentNode != null)
+            {
+                //populate left child node's next pointer = right child node
+                currentNode.left.next = currentNode.right;
+
+                //if there is a next node, we haven't reached the last node in the level
+                if (currentNode.next != null)
+                {
+                    //set the next pointer of the right child node = left child node of node on current level in right subtree
+                    currentNode.right.next = currentNode.next.left;
+                }
+                //advance current node to next node on current level. If next node is null, while loop doesn't execute and method ends
+                currentNode = currentNode.next;
+
+            }
+        }
+    }
+}
--- BinaryTrees/BinaryTrees/Program.cs
+++ BinaryTrees/BinaryTrees/Program.cs
@@ -34,22 +34,33 @@ namespace BinaryTrees
             int[] inorder = new int[] { 9, 3, 15, 20, 7 };
             int[] postorder = new int[] { 9, 15, 7, 20, 3 };
             */
-            //Construct Tree given Preorder and Inorder Test
+            /*Construct Tree given Preorder and Inorder Test
             int[] preorder = new int[] { 3, 9, 20, 15, 7 };
             int[] inorder = new int[] { 9, 3, 15, 20, 7 };
+            */
+            //Populate Next Pointers in a perfect Binary Tree
+            Node node7 = new Node(7, null, null, null);
+            Node node6 = new Node(6, null, null, null);
+            Node node5 = new Node(5, null, null, null);
+            Node node4 = new Node(4, null, null, null);
+            Node node3 = new Node(3, node5, node7, null);
+            Node node2 = new Node(2, node4, node5, null);
+            Node node1 = new Node(1, node2, node3, null);
 
             MaximumDepth maxDepth = new MaximumDepth();
             IsSymmetric symmetric = new IsSymmetric();
             PathSum pathSum = new PathSum();
             GivenInorderAndPostorder buildTree1 = new GivenInorderAndPostorder();
             GivenPreorderAndInorder buildTree2 = new GivenPreorderAndInorder();
+            PopulatingNextRightPointer populateNext = new PopulatingNextRightPointer();
 
 
             //Console.WriteLine("Max depth of tree: " + maxDepth.MaxDepth(node1));
             //Console.WriteLine("Binary Tree is symmetric: " + symmetric.CheckSymmetry(node1));
             //Console.WriteLine("Binary Tree has root to leaf path with sum: " + pathSum.HasPathSum(node1, 22));
             //Console.WriteLine("Constructing Binary Tree...\nRoot node: " + buildTree1.BuildTree(inorder, postorder).val);
-            Console.WriteLine("Constructing Binary Tree...\nRoot node: " + buildTree2.BuildTree(preorder, inorder).val);
+            //Console.WriteLine("Constructing Binary Tree...\nRoot node: " + buildTree2.BuildTree(preorder, inorder).val);
+            Console.WriteLine("Populating Next Pointers of a perfect Binary Tree...\n" + populateNext.Connect(node1).val);
 
             Console.Read();
         }
