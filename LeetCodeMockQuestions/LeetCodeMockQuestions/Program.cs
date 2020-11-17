c469189fd752ed45bbf5b7491e5fb7760af4ca74
James Gilsenan PC <jamesgilsenan37@gmail.com>
2020-11-17 14:33:43 +0000

    LeetCode Mock Questions. ShiftChar and MajorityElement questions

---
+++ LeetCodeMockQuestions/LeetCodeMockQuestions/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LeetCodeMockQuestions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //For testing shiftChar
+            string a = "abcde";
+            string b = "deabc";
+
+            //For testing MajorityElement
+            int[] nums = { 3, 2, 3 };
+
+            ShiftCharQ shiftChar = new ShiftCharQ();
+            MajorityElementQ majorityElement = new MajorityElementQ();
+
+            Console.WriteLine("String A will equal String B: " + shiftChar.ShiftCharQuestion(a, b));
+            var list = majorityElement.MajorityElement(nums);
+            Console.Write("[");
+            foreach (int num in list)
+                Console.Write(num + ", "); ;
+            Console.Write("]");
+
+
+
+            Console.Read();
+        }  
+    }
+}
