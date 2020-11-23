5152a593f71b95b77ac5f04ddf4c1b7c7687739e
James Gilsenan PC <jamesgilsenan37@gmail.com>
2020-11-23 18:40:09 +0000

    WIP TrimBST and AddTwoNumbers Qs

---
+++ LeetCodeMockQuestions/LeetCodeMockQuestions/AddTwoNumbersQ.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeMockQuestions
+{
+    class AddTwoNumbersQ
+    {
+        //You are given two non-empty linked lists representing two non-negative integers. The most significant digit comes first and each of their nodes contain a single digit. 
+        //Add the two numbers and return it as a linked list.
+        //You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+        //Follow up:
+        //What if you cannot modify the input lists? In other words, reversing the lists is not allowed.
+
+        ListNode temp = new ListNode(0);
+
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            ListNode l1Prev = l1;
+            ListNode l2Prev = l2;
+
+            reverseRecusively(l1);
+            reverseRecusively(l2);
+
+            DisplayList(l1);
+            DisplayList(l2);
+
+            return l1;
+        }
+
+        public ListNode reverseRecusively(ListNode node)
+        {
+            
+            if (node.next == null)
+                return node;
+            reverseRecusively(node);
+            temp = node.next;
+            temp.next = node;
+            node.next = null;
+
+            return node;
+        }
+
+        public void DisplayList(ListNode node)
+        {
+            while (node != null)
+            {
+                if (node.next != null)
+                    Console.WriteLine(node.val + " -> ");
+                else
+                    Console.WriteLine(node.val);
+            }
+        }
+    }
+}
--- LeetCodeMockQuestions/LeetCodeMockQuestions/Program.cs
+++ LeetCodeMockQuestions/LeetCodeMockQuestions/Program.cs
@@ -13,8 +13,17 @@ namespace LeetCodeMockQuestions
             //For testing MajorityElement
             int[] nums = { 3, 2, 3 };
 
+            ListNode l1node4 = new ListNode(3, null);
+            ListNode l1node3 = new ListNode(4, l1node4);
+            ListNode l1node2 = new ListNode(2, l1node3);
+            ListNode l1node1 = new ListNode(7, l1node2);
+            ListNode l2node3 = new ListNode(6, null);
+            ListNode l2node2 = new ListNode(5, l2node3);
+            ListNode l2node1 = new ListNode(7, l2node2);
+
             ShiftCharQ shiftChar = new ShiftCharQ();
             MajorityElementQ majorityElement = new MajorityElementQ();
+            AddTwoNumbersQ addTwoNumbers = new AddTwoNumbersQ();
 
             Console.WriteLine("String A will equal String B: " + shiftChar.ShiftCharQuestion(a, b));
             var list = majorityElement.MajorityElement(nums);
@@ -22,6 +31,7 @@ namespace LeetCodeMockQuestions
             foreach (int num in list)
                 Console.Write(num + ", "); ;
             Console.Write("]");
+            addTwoNumbers.AddTwoNumbers(l1node1, l2node1);
 
 
 
