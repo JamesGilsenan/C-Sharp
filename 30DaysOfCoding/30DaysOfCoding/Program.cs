962a3a846662b050249f77473e0735c102ba87a0
James Gilsenan PC <jamesgilsenan37@gmail.com>
2020-12-01 19:40:28 +0000

    Completed DayThree of 30 days of coding

--- 30DaysOfCoding/30DaysOfCoding/Program.cs
+++ 30DaysOfCoding/30DaysOfCoding/Program.cs
@@ -58,5 +58,27 @@ namespace _30DaysOfCoding
             double totalCost = meal_cost + ((meal_cost / 100) * tip_percent) + ((meal_cost/100) * tax_percent);
             Console.WriteLine(Math.Round(totalCost));
         }
+
+        static void DayThree()
+        {
+            /*Given an integer, , perform the following conditional actions:
+            If  is odd, print Weird
+            If  is even and in the inclusive range of 2 to 5, print Not Weird
+            If  is even and in the inclusive range of 6 to 20, print Weird
+            If  is even and greater than 20, print Not Weird */
+
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            if (N % 2 == 1)
+                Console.WriteLine("Weird");
+            else if (N >= 2 && N <= 5 && N % 2 == 0)
+                Console.WriteLine("Not Weird");
+            else if (N >= 6 && N <= 20 && N % 2 == 0)
+                Console.WriteLine("Weird");
+            else if (N > 20 && N % 2 == 0)
+                Console.WriteLine("Not Weird");
+
+
+        }
     }
 }
